f93204a1e5827fdde4375651b5c7c3eb8a3f6c37
ViniciusMateos <vinimateos.s@gmail.com>
2023-11-15 00:15:26 -0300

    Desenvolvimento das telas

---
+++ AjudaCertaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AjudaCertaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AjudaCertaApp/Views/CadastroEscolha.xaml.cs
@@ -0,0 +1,14 @@
+namespace AjudaCertaApp.Views;
+
+public partial class CadastroEscolha : ContentPage
+{
+	public CadastroEscolha()
+	{
+		InitializeComponent();
+	}
+
+    private void Image_SizeChanged(object sender, EventArgs e)
+    {
+
+    }
+}

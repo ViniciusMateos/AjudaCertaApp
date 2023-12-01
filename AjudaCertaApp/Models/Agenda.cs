7e2ec6038cbfb1366fbced635ba3152922f97e84
Seu nome <email@dominio.com>
2023-12-01 11:29:50 -0300

    Listagem de Doacoes inicializada

---
+++ AjudaCertaApp/Models/Agenda.cs
@@ -0,0 +1,19 @@
+﻿using AjudaCertaApp.Models.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjudaCertaApp.Models
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public StatusDoacaoEnum Status { get; set; }
+        public Pessoa Pessoa { get; set; }
+        public Endereco Endereco { get; set; }
+        public Doacao Doacao { get; set; }
+    }
+}
---
+++ AjudaCertaApp/ViewModels/Perfil/ListagemDoacoesViewModel.cs
@@ -0,0 +1,57 @@
+﻿using AjudaCertaApp.Models;
+using AjudaCertaApp.Models.Enuns;
+using AjudaCertaApp.Services.Doacoes;
+using AjudaCertaApp.Services.Pessoas;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjudaCertaApp.ViewModels.Perfil
+{
+    public class ListagemDoacoesViewModel : BaseViewModel
+    {
+        private DoacaoService dService;
+        private PessoaService pService;
+        private string _token;
+        public ObservableCollection<Models.Doacao> Doacoes { get; set; }
+        public ListagemDoacoesViewModel()
+        {
+            _token = Preferences.Get("UsuarioToken", string.Empty);
+            dService = new DoacaoService(_token);
+            pService = new PessoaService(_token);
+            Doacoes = new ObservableCollection<Models.Doacao>();
+
+            _ = ObterDoacoes();
+        }
+
+        #region Métodos
+        public async Task ObterDoacoes()
+        {
+            try
+            {
+                Pessoa p = await pService.GetPessoaPorUsuarioAsync();
+
+                if (p.Tipo == TipoPessoaEnum.ONG)
+                {
+                    Doacoes = await dService.GetDoacoesAsync();
+                    OnPropertyChanged(nameof(Doacoes));
+                }
+                else if(p.Tipo == TipoPessoaEnum.DOADOR)
+                {
+                    Doacoes = await dService.GetDoacoesByPessoa();
+                    OnPropertyChanged(nameof(Doacoes));
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+        #endregion
+    }
+}

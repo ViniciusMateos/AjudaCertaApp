a8c27c645b1d88de8e62dd0091cfb4dcb1246889
Vinicius Mateos <vinimateos.s@gmail.com>
2023-12-04 02:02:13 -0300

    Inserção da tela de reservar e agendar doação para beneficiário

--- AjudaCertaApp/App.xaml.cs
+++ AjudaCertaApp/App.xaml.cs
@@ -7,7 +7,7 @@
             InitializeComponent();
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIYVJpR2Nbe05xflZEal9SVBYiSV9jS31SdEVhWHxdeHRSQ2RbWQ==");
 
-            MainPage = new NavigationPage(new Views.Login());
+            MainPage = new NavigationPage(new Views.Login()) ;
         }
     }
 }
